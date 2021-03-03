88a1cbe4531b33c861ef65e1536e1195a7e1f11f
unknown <sivovol.dmitrii1988@gmail.com>
2021-03-03 21:34:28 +0200

    echo Hello, world! in console

--- Main.java
+++ Main.java
@@ -1,24 +1,6 @@
-import java.util.Scanner;
-
-public class Geometry {
+public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите a");
-        int a = scanner.nextInt();
-        if (0 >= a) {
-            System.out.println("Неправильное значение");
-            System.exit(0);
-        }
-        System.out.println("Введите b");
-        int b = scanner.nextInt();
-        if (0 >= b){
-            System.out.println("Неправильное значение");
-            System.exit(0);
-        }
-        int c;
-        c = (a * a) + (b * b);
-        int hupotenyse = c ;
-        System.out.println("Гипотенуза"+ hupotenyse );
+        System.out.println("Hello, world!");
     }
 }
