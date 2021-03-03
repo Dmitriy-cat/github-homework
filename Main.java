cf0083ce9ac46a49b81cee183d295827b02ba206
unknown <sivovol.dmitrii1988@gmail.com>
2021-03-03 21:51:56 +0200

    adding method print static

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
 	
-	public void print(String str){
+	public static void print(String str){
 		System.out.println(str);
 	}
+
 }
